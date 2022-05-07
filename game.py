b6bdd0fe3352911ef30b2f7e15d78a223baad6fc
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2022-05-07 10:39:53 +0100

    Adjusted board

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ class Game():
         self.gamestep = 0
         self.grid_x=15
         self.grid_y=7
-        self.board = [[' ' for i in range(self.grid_x)] for i in range(self.grid_y)]
+        self.board = [['1' for i in range(self.grid_x)] for i in range(self.grid_y)]
+        self.player_1_funds = 5
+        self.player_2_funds = 5
 
     def print_board(self) -> None:
         # First row
@@ -14,27 +16,29 @@ class Game():
         game='~'
         c=65
         for j in range(self.grid_x):
-            print(f"|{j+1:2} ", end='')
+            print(f"|{j+1:5} ", end='')
         print(f"| ", end='')
         print()
         print(f'{(self.grid_x*4+4)*"-"}')
-
         # Other rows
         for i in range(self.grid_y):
             print(f"{chr(c+i)} ", end='')
             for j in range(self.grid_x):
-                print(f"| {self.board[i][j]} ", end='')
+                print(f"| {self.board[i][j]:4} ", end='')
             print(f"| ", end='')
             print()
-            print(f'{(self.grid_x*4+4)*"-"}')
+            print(f'{(self.grid_x*7+4)*"-"}')
 
 
 
 class Piece():
     def __init__(self) -> None:
+        self.char='A'
+        self.side='w'
         pass
+    def __print_character(self) ->str:
+        return self.char+self.side
 
 if __name__=='__main__':
     myGame = Game()
     myGame.print_board()
-    
